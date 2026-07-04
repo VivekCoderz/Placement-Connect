470d57cc759024621e2e13a3d761f9133e6a5156
Vivek Garg <dayanandgarg967@gmail.com>
2026-07-04 11:58:15 +0530

    Delete backend directory

--- backend/routes/analytics.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const analyticsController = require("../controllers/analytics");
-const { jwtVerify } = require("../middleware/jwtVerify");
-
-router.get("/placement-stats", jwtVerify, analyticsController.getPlacementStats);
-
-module.exports = router;
