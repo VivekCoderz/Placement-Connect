9e0d97a97991aa96d7ba9e349ad23c9e8601444b
Tripti129 <triptimittal996@gmail.com>
2026-07-07 10:32:59 +0530

    delete all folders

--- backend/routes/company.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const companyController = require("../controllers/company");
-const { jwtVerify, authorizeRoles, getAssociatedDetails } = require("../middleware/jwtVerify");
-
-router.post("/jobs", jwtVerify, authorizeRoles("recruiter", "company"), getAssociatedDetails, companyController.postJob);
-router.get("/applications/:jobId", jwtVerify, authorizeRoles("recruiter", "company"), getAssociatedDetails, companyController.getApplications);
-
-module.exports = router;
